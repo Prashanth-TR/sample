be2a3054e3b9e9becd46b1f7f04d5f03da40bee2
Anirudh <ranirud@hotmail.com>
2019-07-10 11:10:53 +0530

    Anirudh uploads a java file

---
+++ MasterMind.java
@@ -0,0 +1,17 @@
+public class MasterMind {
+    public static void main(String[] args) {
+        int x, t;
+        for (int i=1; i<10; i++)
+            for (int j = 1; j < 10; j++)
+                for (int k = 0; k < 10; k++) {
+                    x = i * 100 + j * 10 + k;
+                    t = k * 100 + k * 10 + k;
+                    x = 3 * x;
+                    if (x == t && i != j && j != k) {
+                        System.out.println(i);
+                        System.out.println(j);
+                        System.out.println(k);
+                    }
+                }
+    }
+}
